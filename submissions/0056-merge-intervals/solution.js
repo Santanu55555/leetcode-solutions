6bcc609b6a3ed89c1dcd5217c9a9a47714da9c63
Santanu Mohapatra <revyor@Santanus-MacBook-Air.local>
2026-04-26 10:15:23 +0000

    [LeetCode Sync] Runtime - 7 ms (82.36%), Memory - 64.9 MB (52.81%)

--- submissions/0056-merge-intervals/solution.js
+++ submissions/0056-merge-intervals/solution.js
@@ -3,21 +3,15 @@
  * @return {number[][]}
  */
 var merge = function(intervals) {
-  intervals.sort((a, b) => a[0] - b[0]);
-
-  let res = [intervals[0]];
-
-  for (let i = 1; i < intervals.length; i++) {
-    let last = res[res.length - 1];
-    let curr = intervals[i];
-
-    if (curr[0] <= last[1]) {
-      last[1] = Math.max(last[1], curr[1]);
-    } else {
-      res.push(curr);
+  intervals.sort((a,b)=>a[0]-b[0])
+  let res=[intervals[0]]
+  for(let i=1;i<intervals.length;i++){
+    if(res[res.length-1][1]>=intervals[i][0]){
+        res[res.length-1][1] = Math.max(res[res.length-1][1],intervals[i][1])
+    }
+    else{
+        res.push(intervals[i])
     }
   }
-
-  return res;
-
+  return res
 };
